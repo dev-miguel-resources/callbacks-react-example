b9255aa99625d816e7ae6066197c4feb82b5f52b
dev-miguel-resources <ingenieromiguelch@gmail.com>
2022-12-08 08:20:15 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./global.css";
+import Car from "./components/Car/Car";
+
+export function App() {
+
+  function hello() {
+    
+  }
+
+  return (
+    <div>
+      <p>I am the {"<App />"}</p>
+      <Car />
+    </div>
+  );
+}
+
+
---
+++ src/components/Car/Car.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Car = () => {
+  return <div>Hello am Car</div>;
+};
+
+export default Car;
